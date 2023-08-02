2565a1fcceb0042b58844e91a4f533c38e60764f
Emi295 <80912574+Emi295@users.noreply.github.com>
2023-08-02 00:06:11 +0000

    mostrar estudiantes

--- escuela_api/api.py
+++ escuela_api/api.py
@@ -1,7 +1,17 @@
 import flask
+import csv
 
 app = flask.Flask(__name__)
 
-@app.route("/hola_mundo")
-def hola_mundo():
-    return "<h1>Hola mundo</h1>"
+nombre_archivo = "datos/estudiante.csv" /workspaces/ista-python-curso-2023/datos/estudiante.csv
+with open(nombre_archivo, "r") as estudiantes:
+    lector = csv.reader(estudiantes, delimiter=",")
+    # Omitir el encabezado
+    next(lector, None)
+    for fila in lector:
+        # Tenemos la lista. En la 0 tenemos el nombre, en la 1 la calificación y en la 2 el precio
+        nombre = fila[0]
+        calificacion = int(fila[1])
+        precio = float(fila[2])
+        print(
+            f"Juego: '{nombre}' con calificación {calificacion} y precio {precio}")

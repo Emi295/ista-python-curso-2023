ac9a1fe4eb7f997d4ed839f4aba8504be1ac0c7e
Emi295 <80912574+Emi295@users.noreply.github.com>
2023-08-02 03:23:20 +0000

    Agregar Asistencia

--- escuela_api/app.py
+++ escuela_api/app.py
@@ -3,18 +3,31 @@ import csv
 
 app = flask.Flask(__name__)
 
-@app.route("/lista_alumnos")
-def mostrar_listaAlumnos():
-    
+
+@app.route("/hola_mundo")
+def hola_mundo():
+    return "<h1>Hola mundo</h1>"
+          
+@app.route("/mostrar_alumnos")
+def lista_alumnos():
     nombre_archivo = "/workspaces/ista-python-curso-2023/datos/estudiante.csv"
-    with open(nombre_archivo, "r") as estudiantes:
+    with open(nombre_archivo,'r') as estudiantes:
         lector = csv.reader(estudiantes, delimiter=",")
-        # Omitir el encabezado
-        next(lector, None)
+        #Omite los encabezados
+        next(lector,None)
         for fila in lector:
-            # Tenemos la lista. En la 0 tenemos el nombre, en la 1 la calificación y en la 2 el precio
             nombre1 = fila[3]
-            nombre2 = fila[4]
+            nombre2 = fila[4] 
             apellido1= fila[1]
             apellido2= fila[2]
-      return( f"'{nombre1}{nombre2}{apellido1}{apellido2}")
+    # return f'{nombre1}{nombre2}{apellido1}{apellido2}'
+    return fila
+
+@app.route("/registrar_asistencia", methods=["POST"])
+def agregar_asistencia():
+    datos = flask.request.json
+    lista = [valor for valor in datos.values()]
+    datos_ingresados = "\n" + ",".join(lista)
+    with open("/workspaces/ista-python-curso-2023/datos/asistencia.csv", "a") as asistencia:
+        asistencia.write(datos_ingresados)
+    return "Asistencia registrada con exito"

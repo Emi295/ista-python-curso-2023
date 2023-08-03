61ec7b3354e1d7b44eb9c24d673c32592c20262e
Emi295 <80912574+Emi295@users.noreply.github.com>
2023-08-03 21:46:43 +0000

    cambios

--- escuela_api/app.py
+++ escuela_api/app.py
@@ -15,13 +15,15 @@ def lista_alumnos():
         lector = csv.reader(estudiantes, delimiter=",")
         #Omite los encabezados
         next(lector,None)
+        filas = {}
         for fila in lector:
             nombre1 = fila[3]
             nombre2 = fila[4] 
             apellido1= fila[1]
             apellido2= fila[2]
+            filas.append((nombre1,nombre2,apellido1,apellido2))
     # return f'{nombre1}{nombre2}{apellido1}{apellido2}'
-    return fila
+    return filas
 
 @app.route("/registrar_asistencia", methods=["POST"])
 def agregar_asistencia():
@@ -30,4 +32,10 @@ def agregar_asistencia():
     datos_ingresados = "\n" + ",".join(lista)
     with open("/workspaces/ista-python-curso-2023/datos/asistencia.csv", "a") as asistencia:
         asistencia.write(datos_ingresados)
+    return "Asistencia registrada con exito"
+
+
+@app.route("/total_asistencias")
+def contar_asistencia():
+
     return "Asistencia registrada con exito"
